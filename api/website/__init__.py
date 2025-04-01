beeacae0b2f771364cc48b7fc9d14d0625a204e8
MrViper111 <mrviper118@gmail.com>
2025-04-01 15:45:32 -0700

    stuff

--- api/website/__init__.py
+++ api/website/__init__.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask
+import flask
 from .views import views
 
 
 def create_app():
-    app = Flask(__name__)
+    app = flask.Flask(__name__)
     app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
     app.register_blueprint(views, url_prefix="/")
 
